5e2c035745a184d79764bbf97da2f4cecd3d274f
Tom Soler <tom.a.soler@outlook.com>
2024-02-05 19:44:39 +0100

    commit des fichiers sass

--- motaphoto/functions.php
+++ motaphoto/functions.php
@@ -1,15 +1,20 @@
 <?php
-function enqueue_custom_styles() {
-    wp_enqueue_style('custom-styles', get_template_directory_uri() . '/styles/style.css');
-  }
-  
-  add_action('wp_enqueue_scripts', 'enqueue_custom_styles');
-  
+function theme_enqueue_styles() {
+  // Ajout du style du thème parent
+  wp_enqueue_style('parent-style', get_template_directory_uri() . '/style.css');
+
+  // Ajout du style personnalisé généré à partir de Sass, dépendant du style du thème parent
+  wp_enqueue_style('custom-style', get_template_directory_uri() . '/css/styles.css', array('parent-style'), '1.0.0', 'all');
+}
+add_action('wp_enqueue_scripts', 'theme_enqueue_styles');
+
 
 function enqueue_custom_script() {
     // Enfilet le script personnalisé
-    wp_enqueue_script('custom-script', get_stylesheet_directory_uri() . '/js/custom-script.js', array('jquery'), '1.0.0', true);
+    wp_enqueue_script('custom-script', get_template_directory_uri() . '/js/custom-script.js', array('jquery'), '1.0.0', true);
 }
 add_action('wp_enqueue_scripts', 'enqueue_custom_script');
 
-register_nav_menus(array('main'=>'menu'));
+
+
+register_nav_menus(array('menu'=>'menu-header'));
